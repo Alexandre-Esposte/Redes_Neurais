c2356641e7f47d9fdb333ae4a633551f70c46b44
Alexandre Esposte <alexandreesposte@gmail.com>
2023-02-10 22:27:47 -0300

    mais organizado

--- adaline/adaline.py
+++ adaline/adaline.py
@@ -9,11 +9,9 @@ class Adaline:
 		self.tolerancia = tolerancia
 		self.qt_cols = 0
 		self.qt_rows = 0
-		self.limiar_ativacao = 0
 		self.pesos = np.array([])
 		self.limiar_ativacao = limiar_ativacao
 		self.histpeso = {}
-		self.p = 0
 		self.histerr = {}
 
 	
@@ -30,21 +28,23 @@ class Adaline:
 		self.qt_rows = X.shape[0]
 		total = X.size
 
-		self.p = self.qt_rows
 
-
+		#Iniciando o vetor de pesos
 		self.pesos = np.random.rand(self.qt_cols)
 
-
+		#Iniciando o limiar
 		limiar = np.array([self.limiar_ativacao])
-		self.pesos = np.hstack([limiar,self.pesos])
+		
+		#Adicionando o limiar na primeira posição do vetor de pesos
+		self.pesos = np.hstack([self.limiar_ativacao,self.pesos])
 
+		#Adicionando o valor -1 no inicio de todas as linhas da matriz que contém as váriveis utilizadas para o treinamento
 		X = np.hstack([-1*np.ones(self.qt_rows).reshape(-1,1),X])
 
 		#Iniciando contador de epoca
 		epoch = 0
 
-		#erros
+		#Iniciando os erros
 		erro_agora, erro_anterior = 1, 0
 
 		# Iniciando o processo de treinamento
@@ -55,25 +55,39 @@ class Adaline:
 			erro_anterior = erro_agora
 			err = 0
 			vetor_erro_acumulado = np.zeros(self.qt_cols+1)
+
 			#Ponderando a entrada com os pesos
 			for i in range(self.qt_rows):
 				# Realizando a soma ponderada
-				sum_ = (X[i]*self.pesos).sum()
+				u = (X[i]*self.pesos).sum()
 
 				# atualizando os pesos
 				#self.pesos = self.pesos + (self.taxa_aprendizado * (label[i] - sum_) * X[i])
 				
-				vetor_erro_acumulado = vetor_erro_acumulado + ((label[i] - sum_)* X[i])
-				err = err + (label[i] - sum_)**2
+				#Incrementando o vetor com os erros acumulado após cada registro
+				vetor_erro_acumulado = vetor_erro_acumulado + ((label[i] - u)* X[i])
+				
+				#Erro quadratico após cada registro
+				err = err + (label[i] - u)**2
 
 
 
 			# atualizando os pesos através do erro acumulado
 			self.pesos = self.pesos + (self.taxa_aprendizado * vetor_erro_acumulado)
-			erro_agora = err/self.p
+
+			# Ero quadrático médio em cada época
+			erro_agora = err/self.qt_rows
+
+			# Incrementando a época
 			epoch += 1
+
+			#Printando os pesos por época e seu erro comparado ao erro passado
 			print(f"Peso Epoch {epoch}: {self.pesos} Erro quadratico: {np.abs(erro_agora - erro_anterior)	}")
+
+			# Adicionando os pesos no dicionario de pesos em cada época
 			self.histpeso[f"{epoch}"] = self.pesos
+
+			# Adicionando a diferença de erro de cada época no dicionario de erros
 			self.histerr[epoch] = np.abs(erro_agora - erro_anterior)	
 
 
@@ -83,6 +97,7 @@ class Adaline:
 
 	def predict(self,X):
 		'''Realizando predições com a rede neural'''
+
 
 		X = np.hstack([-1*np.ones(X.shape[0]).reshape(-1,1),X])
 		
